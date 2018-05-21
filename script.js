0350af81fd9be6fdbc388770ba578056c12b5acc
Gyozmo <binantguillaume@gmail.com>
2018-05-21 10:30:38 +0200

    jquery code + importing jquery ui

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 $( "#mozaic" ).children().mouseenter(function(){
-    $(this).toggleClass('test');
-    
-        
+    // $(this).css('width','300%');
+    // $(this).css('z-index','999999');
+    $(this).effect('shake');
+
+
       });
 
       $( "#mozaic" ).children().mouseleave(function(){
-        $(this).toggleClass('test');
-        
-            
+        // $(this).css('width','100%');
+        // $(this).css('z-index','0');
+
+
           });
