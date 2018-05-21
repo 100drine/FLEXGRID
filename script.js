36a5f02d5d3920a6fbba5dfc74d25ec8db8b3c4a
Gyozmo <binantguillaume@gmail.com>
2018-05-21 11:19:45 +0200

    jquery ui works but cant find a good way to animate...

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-$( "#mozaic" ).children().mouseenter(function(){
+$( "#mozaic" ).children().click(function(){
     // $(this).css('width','300%');
     // $(this).css('z-index','999999');
     // $(this).effect('shake');
-    $(this).toggle('puff','10%');
+    $(this).toggle( "size", {
+    to: { width: 200, height: 60 }
+  }, 1000);
 
 
       });
 
-      $( "#mozaic" ).children().mouseleave(function(){
+      $( "#mozaic" ).children().click(function(){
         // $(this).css('width','100%');
         // $(this).css('z-index','0');
-$(this).toggle('puff','10%');
+$(this).toggle('size');
 
           });
