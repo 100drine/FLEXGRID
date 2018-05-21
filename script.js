0aacba76042d33d2226d3fef9562a4f8681c8421
Gyozmo <binantguillaume@gmail.com>
2018-05-21 11:51:57 +0200

    tried a flip effect but not working for the moment, eating time!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$( "#mozaic" ).children().click(function(){
+$( "#mozaic" ).children().mouseenter(function(){
     // $(this).css('width','300%');
     // $(this).css('z-index','999999');
     // $(this).effect('shake');
@@ -9,9 +9,35 @@ $( "#mozaic" ).children().click(function(){
 
       });
 
-      $( "#mozaic" ).children().click(function(){
+      $( "#mozaic" ).children().mouseleave(function(){
         // $(this).css('width','100%');
         // $(this).css('z-index','0');
 $(this).toggle('size');
 
           });
+
+
+
+// $( "#box30" ).mouseenter(function(){
+// $('#box30').flip(true);
+// console.log($(this));
+//   });
+//
+//   $( "#box30" ).mouseleave(function(){
+//   $('#box30').flip(false);
+//   console.log($(this));
+//     });
+//
+//     $("#box30").flip({
+//       axis: 'x',
+//       trigger: 'hover'
+//     });
+//
+//
+//
+//
+//
+//
+// $('document').ready(function(){
+//   console.log($('#box30'));
+// })
